e741910fc1da2387a321114975af4ac916892dec
sondemigods <a13752365641@126.com>
2021-08-17 09:32:34 +0800

    feat:添加主题配置。

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,12 @@
 import url from './url.config'
 import layout from './layout.config'
+import theme from './theme.config'
 
 export default {
   // 路径配置
   ...url,
   // 布局配置
   layout,
+  // 主题配置
+  theme,
 }
---
+++ src/config/theme.config.ts
@@ -0,0 +1,5 @@
+export default {
+  navBgColor: '#545c64',
+  navTextColor: '#ffffff',
+  navActiveTextColor: '#ffd04b'
+}
