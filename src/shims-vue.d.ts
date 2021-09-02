980d54d507784fc1631851391aa479334b1992e8
sondemigods <a13752365641@126.com>
2021-09-02 15:39:54 +0800

    feat:添加404页面。

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -13,3 +13,8 @@ declare module '*.jpg' {
   const classes: { readonly [key: string]: string }
   export default classes
 }
+
+declare module '*.svg' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
--- src/views/System/Error/404.vue
+++ src/views/System/Error/404.vue
@@ -1,18 +1,72 @@
 <template>
-  <div>404</div>
+  <div>
+    <img :src="error404"
+         class="error-bg" />
+    <div class="error-btn-box">
+      <el-button @click="routeGoHome">返回首页</el-button>
+      <el-button type="primary"
+                 @click="routeGoback">返回上一页</el-button>
+    </div>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
 // 导入vue功能
 import {
   defineComponent
 } from 'vue'
 
+// 导入路由功能
+import {
+  useRouter
+} from 'vue-router'
+
+// 导入图片
+import error404 from '@/assets/error-404.svg'
+
 export default defineComponent({
   name: '404',
   props: {},
-  setup () {}
+  setup() {
+    const router = useRouter()
+
+    /**
+     * @functionName routeGoHome
+     * @description 返回首页
+     * @author 张航
+     * @date 2021-09-02 13:55:48
+     * @version V1.0.0
+     */
+    const routeGoHome = (): void => {
+      router.replace({ path: '/' })
+    }
+
+    /**
+     * @functionName routeGoback
+     * @description 返回上一页
+     * @author 张航
+     * @date 2021-09-02 13:56:47
+     * @version V1.0.0
+     */
+    const routeGoback = (): void => {
+      router.back()
+    }
+
+    return {
+      error404,
+      routeGoHome,
+      routeGoback
+    }
+  }
 })
 </script>
 <style lang="scss" scoped>
+.error-bg {
+  display: block;
+  width: 40%;
+  margin: 6% auto;
+}
+.error-btn-box {
+  text-align: center;
+}
 </style>
