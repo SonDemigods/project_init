4de6d984202bc2ac6651b8fc484e5098b27358fc
sondemigods <a13752365641@126.com>
2021-09-09 16:44:02 +0800

    feat:编写文件生成demo。

--- src/views/Business/InitPageByVue/index.vue
+++ src/views/Business/InitPageByVue/index.vue
@@ -1,20 +1,55 @@
 <template>
   <div>
-    vue
+    <el-button type="primary"
+               @click="exportProject">生成项目</el-button>
   </div>
 </template>
 
 <script lang="ts">
 // 导入vue功能
-import {
-  defineComponent
-} from 'vue'
+import { defineComponent } from 'vue'
+
+import { showOpenDialog } from '@/tools/electron.tool'
+
+import { writeFile } from '@/tools/template.tool'
 
 export default defineComponent({
   name: 'InitPageByVue',
   components: {},
   props: {},
-  setup () {}
+  setup() {
+    /**
+     * @functionName selectPathHandle
+     * @description 打开路径选择面板
+     * @author 张航
+     * @date 2021-09-09 16:37:35
+     * @version V1.0.0
+     */
+    const selectPathHandle = (): void => {
+      showOpenDialog().then((res) => {
+        if (!res.canceled) {
+          writeFile(res.filePaths[0], 'build')
+        } else {
+          console.log('取消操作！')
+        }
+      })
+    }
+
+    /**
+     * @functionName exportProject
+     * @description 方法描述
+     * @author 张航
+     * @date 2021-09-09 16:36:09
+     * @version V1.0.0
+     */
+    const exportProject = (): void => {
+      selectPathHandle()
+    }
+
+    return {
+      exportProject
+    }
+  }
 })
 </script>
 <style lang="scss" scoped>
